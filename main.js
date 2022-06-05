338053447fcac6a606723484502004f2af10f336
Emma Remon <90062286+ecs3edec3ex232@users.noreply.github.com>
2022-06-05 16:38:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0; 
+
+function preload(){
+clown_nose = loadImage("https://i.postimg.cc/qqbBgBtH/clown-nose.png");
+}
+
+function setup(){
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function Take_snapshot(){
+    save('Photo_with_TINT.png');
+}
+
+function modelLoaded(){
+    console.log("posenet is initialised");
+}
+
+function gotPoses(results){
+    if( results.length > 0 ){
+console.log(results);
+noseX = results[0].pose.nose.x - 15;
+noseY = results[0].pose.nose.y - 15;
+console.log("nose x =" + noseX);
+console.log("nose y =" + noseY);
+    }
+}
